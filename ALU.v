446c1e6ad4446a680c798ba31d40f0b909be6a9e
jgaudet <jgaudet@hammer.eece.maine.edu>
2009-10-27 04:43:17 -0400

    more changes to blocks

--- ALU.v
+++ ALU.v
@@ -6,27 +6,27 @@
 //111 set on less than
 //2 32 bit bus in and one out
 
-module ALU (input [2:0] operation,input [31:0] x, input [31:0] y, input shamt, output reg [31:0] RESULT, output reg zero);
-//reg [31:0] RESULT; 
-//assign RES = RESULT;
+module ALU (input [3:0] operation,input [31:0] x, input [31:0] y, input [4:0] shamt, output reg [31:0] RESULT, output reg zero);
+
 always @(x,y,operation, shamt)
 begin
 case(operation)
-	3'b000 : RESULT = x & y;
-	3'b001 : RESULT = x | y;
-	3'b010 : RESULT = x + y;
-	3'b011 : RESULT = ~(x | y);
-	3'b100 : RESULT = x << shamt;
-	3'b101 : RESULT = x >> shamt;
-	3'b110 : RESULT = x - y;
+	4'b0000 : RESULT = x & y;
+	4'b0001 : RESULT = x | y;
+	4'b0010 : RESULT = x + y;
+	4'b0011 : RESULT = ~(x | y);
+	4'b0100 : RESULT = x << shamt;
+	4'b0101 : RESULT = x >> shamt;
+	4'b0110 : RESULT = x - y;
 	
-	3'b111 : 
+	4'b0111 : 
 		begin
 			if ((x - y)>0)
 			zero=1'b1;
 			else
 			zero=1'b0;
 		end
+	4'b1000 : RESULT = x>>>shamt;
 	default;
 endcase
 end
--- pc.v
+++ pc.v
@@ -1,7 +1,11 @@
-module pc (	input [31:0] ADDR,	input wire clk,	output reg [31:0] PC);	
+module pc (	input [31:0] ADDR,	input wire clk, input wire reset, output reg [31:0] PC);	
 always @ (posedge clk)
 begin
 	PC = ADDR;
+end
+always @ (reset)
+begin
+	PC = 0;
 end
 
 endmodule 
